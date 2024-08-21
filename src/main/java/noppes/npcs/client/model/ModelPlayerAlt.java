04911ea7d751a2fd516d97b6872475d345ac04f0
BetaZavr <75635959+BetaZavr@users.noreply.github.com>
2024-08-21 08:11:36 +0300

    added class model

---
+++ src/main/java/noppes/npcs/client/model/ModelPlayerAlt.java
@@ -0,0 +1,11 @@
+package noppes.npcs.client.model;
+
+// Used for third party mods trying to integrate with CNPCs
+public class ModelPlayerAlt
+        extends ModelNpcAlt {
+
+    public ModelPlayerAlt(float scale, boolean arms) {
+        super(scale, arms, false);
+    }
+
+}
